9b28cb0ceb06cb12695ed4118b68983c34a8c4e8
Egor Serov <you@example.com>
2022-12-04 04:50:37 +0300

    First attempt with wrong answer

--- Furier_method.py
+++ Furier_method.py
@@ -8,47 +8,64 @@
 from math import pi
 from math import cos
 from math import sin
+from math import sinh as sh
+from math import cosh as ch
+from scipy import integrate
 import numpy as np
 import plotly.graph_objs as go
 
-a = 1   # constant from wave equation
-l = 1   # length of the string
-h = 0.01    # u(c,0)
-accuracy = 1e-10
+a = 1  # constant from wave equation
+l = 1  # length of the kernel
+alpha = 0.01  # u(c,0)
+accuracy = 1e-4
 
 x_array = np.linspace(0, l, 1000)  # "x" array with 1000 elements from 0 to l
 
 
-def cosinus(n, t):
-    return cos(pi * (2 * n + 1) * a * t / l)
+def mu_value(n):
+    mu = np.array([1.875, 4.694, 7.854])
+    if n < 4:
+        n -= 1
+        return mu[n]
+    else:
+        return pi / 2 * (2 * n - 1)
 
 
-def sinus(n, x):
-    return sin(pi * (2 * n + 1) * x / l)
+def hyperbolic_function(x, n):
+    arg = mu_value(n) / l * x
+    first_bracket = ch(mu_value(n)) + cos(mu_value(n))
+    second_bracket = sh(arg) - sin(arg)
+    third_bracket = sh(mu_value(n)) + sin(mu_value(n))
+    fourth_bracket = ch(arg) - cos(arg)
+    return alpha * x ** 2 * (first_bracket * second_bracket - third_bracket * fourth_bracket)
 
 
-def constant(n):
-    return 32 * h / (pi * (2 * n + 1)) ** 3
+def a_coefficient(n):
+    return 2 / l * integrate.quad(hyperbolic_function, 0, l, args=(n,))[0]
+
+
+def b_coefficient(n):
+    return 2 * l ** 2 / (a * mu_value(n) ** 2)
 
 
 def answer(x, t, n):
-    return constant(n) * cosinus(n, t) * sinus(n, x)
+    return hyperbolic_function(x, n) / (alpha * x ** 2) * (a_coefficient(n) * cos((mu_value(n) / l) ** 2 * a * t) +
+                                                           b_coefficient(n) * cos((mu_value(n) / l) ** 2 * a * t))
 
 
 def sum_of_n(x, t):
     """
-    вычисляет сумму функции function
+    вычисляет сумму функции answer
     :return:
     """
-    n = 0
-    function = answer(x, t, n)
+    n = 1
+    row_component = answer(x, t, n)
     # print(function)
     summa = 0
-    # TODO: check condition for function(n=0): вроде, работает
-    while abs(function) > accuracy:
-        summa += function
+    while abs(row_component) > accuracy:
+        summa += row_component
         n += 1
-        function = answer(x, t, n)
+        row_component = answer(x, t, n)
         # print(f'function {n} = {function}')
     # print(f'summ = {summ}')
     # print(f'{n} iterations for summ')
@@ -62,8 +79,8 @@ def u(x, t):
     :return: numpy array with u(x,t)
     """
     u_array = np.array([])
-    for i in x:
-        u_array = np.append(u_array, sum_of_n(i, t))
+    for x_coordinate in x:
+        u_array = np.append(u_array, sum_of_n(x_coordinate, t))
     return u_array
 
 
@@ -80,4 +97,5 @@ def get_figure(x, t):
     fig.show()
 
 
-get_figure(x=x_array, t=np.linspace(0, 1, 10))
+get_figure(x=x_array, t=np.linspace(0, 1, 2))
+# print(a_coefficient(3))
